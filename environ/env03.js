c486f5a610c1ba824bbbfb38b52fc2f5a9d6912e
Rosey <rosaharr@ucsc.edu>
2026-05-01 15:09:28 -0700

    kl

---
+++ environ/env03.js
@@ -0,0 +1,22 @@
+let count = 0 
+
+let environmentTitle = "The Mermaid's Adventure";
+
+let environmentElements = ["ocean", "mermaid", "bubbles", "kelp forest"];
+
+$("#needy-button").click(function () { 
+count = count + 1;
+colorCount=count-1;
+
+  $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorCount] );
+   $("body").css("background-color", colors[colorCount]); 
+    else {
+      $("#needy-button").after(" The Mermaid's Adventure ");
+
+let mainEntity = {
+    name: "The Mermaid",
+    type: "soft border",
+    mood: "whimsical",
+    isMoving: true,
+    favoriteElement: waterElements[1]
+}    
