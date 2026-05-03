c5956ef3b1f9899a9b544fc70b8e4459518762c8
Rosey <rosaharr@ucsc.edu>
2026-05-02 20:22:13 -0700

    re

--- environ/env03.js
+++ environ/env03.js
@@ -4,15 +4,24 @@ let environmentTitle = "The Mermaid's Adventure";
 
 let environmentElements = ["ocean", "mermaid", "bubbles", "kelp forest"];
 
-$("#needy-button").click(function () { 
+$("#needy-button").click(function () {
 count = count + 1;
 colorCount=count-1;
 
-  $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorCount] );
-   $("body").css("background-color", colors[colorCount]); 
-    else {
-      $("#needy-button").after(" The Mermaid's Adventure ");
 
+  $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorCount] );
+   $("body").css("background-color", colors[colorCount]);
+    else {
+        $("#needy-button").after(" The Mermaid's Adventure ");
+    }
+let message = "<p>You clicked me " + count + " times.</p>";
+    message = message + "<p> The Mermaid's Adventure" + buttonCreature.favoriteFood + ".</p>";
+
+
+$("#output").html(message);
+
+
+    
 let mainEntity = {
     name: "The Mermaid",
     type: "soft border",
