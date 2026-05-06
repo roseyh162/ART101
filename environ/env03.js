9fd26ce7f416032e532c50540314692d22eabd5a
Rosey <rosaharr@ucsc.edu>
2026-05-06 12:55:41 -0700

    roseys updated

--- environ/env03.js
+++ environ/env03.js
@@ -3,29 +3,30 @@ let count = 0
 let environmentTitle = "The Mermaid's Adventure";
 
 let environmentElements = ["ocean", "mermaid", "bubbles", "kelp forest"];
+let colors = ["lightblue", "lightgreen", "lightpink", "lightseagreen"];
 
 $("#needy-button").click(function () {
-count = count + 1;
-colorCount=count-1;
+    count = count + 1;
+    let colorCount = count - 1;
 
-
-  $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorCount] );
-   $("body").css("background-color", colors[colorCount]);
-    else {
+    if (colorCount < colors.length) {
+        $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorCount]);
+        $("body").css("background-color", colors[colorCount]);
+    } else {
         $("#needy-button").after(" The Mermaid's Adventure ");
     }
-let message = "<p>You clicked me " + count + " times.</p>";
-    message = message + "<p> The Mermaid's Adventure" + buttonCreature.favoriteFood + ".</p>";
 
+    let message = "<p>You clicked me " + count + " times.</p>";
+    message = message + "<p> The Mermaid's Adventure " + mainEntity.favoriteFood + ".</p>";
 
-$("#output").html(message);
+    $("#output").html(message);
+});
 
-
-    
 let mainEntity = {
     name: "The Mermaid",
     type: "soft border",
     mood: "whimsical",
     isMoving: true,
-    favoriteElement: waterElements[1]
+    favoriteFood: "seaweed",
+    favoriteElement: environmentElements[1]
 }    
