c85275198a5165ffb480515e2e871e9e4622310e
Rosey <rosaharr@ucsc.edu>
2026-05-08 19:49:50 -0700

    lB

---
+++ exercises/lab05.js
@@ -0,0 +1,21 @@
+function askNumber(whatNumber) {
+ let userNumber = prompt("Guess 1-10?");
+ 
+ if (userNumber == whatNumber) {
+$("#output").html("You got it!"); 
+ }
+ else {
+   $("#output").html("Noooooope");
+ }
+} 
+
+$("#good-button") .click(function () {
+askNumber(7);
+
+});
+
+
+
+
+
+
