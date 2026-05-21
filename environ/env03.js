eba5bfc0b2c60e65436e04f988c1c1d48f581f18
Rosey <rosaharr@ucsc.edu>
2026-05-21 08:14:02 -0700

    kl

--- environ/env03.js
+++ environ/env03.js
@@ -5,27 +5,31 @@ let environmentTitle = "The Mermaid's Adventure";
 let environmentElements = ["ocean", "mermaid", "bubbles", "kelp forest"];
 let colors = ["lightblue", "lightgreen", "lightpink", "lightseagreen"];
 let count = 0 
-let following = false;
+let following = false ; 
 
+$("#needy-button").click(function () {
+    console.log("needy-button clicked");
+
+    // count = count + 1;
+    // let colorCount = count - 1;
+
+    // if (colorCount < colors.length) {
+    //     $("#good-button").html("Clicks: " + count + " Color: " + colors[colorCount]);
+    //     $("body").css("background-color", colors[colorCount]);
+    // } else {
+    //     $("#good-button").after(" The Mermaid's Adventure ");
+    // }
+
+    // let message = "<p>You clicked me " + count + " times.</p>";
+    // message = message + "<p> The Mermaid's Adventure " + mainEntity.favoriteFood + ".</p>";
+
+    $("#output").html("hjhjhj");
+});
 $("#good-button").click(function () {
     console.log("good-button clicked");
 
-    count = count + 1;
-    let colorCount = count - 1;
-
-    if (colorCount < colors.length) {
-        $("#good-button").html("Clicks: " + count + " Color: " + colors[colorCount]);
-        $("body").css("background-color", colors[colorCount]);
-    } else {
-        $("#good-button").after(" The Mermaid's Adventure ");
-    }
-
-    let message = "<p>You clicked me " + count + " times.</p>";
-    message = message + "<p> The Mermaid's Adventure " + mainEntity.favoriteFood + ".</p>";
-
-    $("#output").html(message);
+    $("#output").html("hjhjhj");
 });
-
 let mainEntity = {
     name: "The Mermaid",
     type: "soft border",
@@ -54,12 +58,12 @@ askNumber("Always a fun time");
 
 
 
-$("#oceancreature").click(function () {
+$("#creature").click(function () {
 $(this).toggleClass("transformed");
-  $("#status").text("The ocean creature changed shape.");
+  $("#status").text("The creature changed shape.");
 }); 
 
-$("#oceancreature").hover(
+$("#creature").hover(
   function () {
     $("#thought").stop(true, true).slideDown(300);
      $("#status").text("The creature revealed a thought.");
@@ -77,19 +81,19 @@ $(document).keydown(function (event) {
     if (event.key === " " || event.code === "Space") {
         event.preventDefault();
          following = !following;
-         $("# ocean creature").toggleClass("following");
+         $("#creature").toggleClass("following");
 
              if (following === true) {
-      $("#status").text("The ocean creature is following your cursor.");
+      $("#status").text("The creature is following your cursor.");
     } else {
-      $("#status").text("The oceancreature stopped following you.");
+      $("#status").text("The creature stopped following you.");
     }
     }
 });
 
 $(document).mousemove(function (event) {
  if (following === true) {
-     $("#oceancreature").css({
+     $("#creature").css({
       left: event.pageX - $("#scene").offset().left + 30,
       top: event.pageY - $("#scene").offset().top + 30
     });
